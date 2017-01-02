97d43379c51f13b0d6a80f79348c9754ca49d814
Jessica Vasquez-Soltero <jmichellevs@gmail.com>
2017-01-02 15:31:22 -0800

    Created user model with hash password method

---
+++ lab-jessica/models/user.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const Schema = mongoose.Schema;
+
+const userSchema = Schema({
+  username: {type: String, required: true, unique: true},
+  email: {type: String, require: true, unique: true},
+  password: {type: String, required: true},
+  findHash: {type: String, required: true}
+});
+
+userSchema.methods.hashPassword = function(password) {
+  return new Promise((resolve, reject) => {
+    bcrypt.hash(password, 10, (err, hash) => {
+      if (err) return reject(err);
+      this.password = hash;
+      resolve(this);
+    });
+  });
+};
+
+module.exports = mongoose.model('user', userSchema);
