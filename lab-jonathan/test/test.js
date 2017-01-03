7a6f906b1a28b5f70f19a470a23b3910ff85a274
spamalope01 <spamalope01@yahoo.com>
2017-01-02 20:23:22 -0800

    can not get tests to work...as usual.

--- lab-jonathan/test/test.js
+++ lab-jonathan/test/test.js
@@ -4,44 +4,42 @@ require('../index.js');
 
 
 describe('testing user rotues', function(){
-  // let owner = null
-  // before((done) => {
-  //   console.log('waiting');
-  //   setTimeout(() => {}, 10000);
-  //   request.post('localhost:9000/owners')
-  //      .send({name: 'OWNER NAME', style: 'OWNER STYLE', guitar: 'OWNER GUITAR'})
-  //      .end((err, res) => {
-  //        owner = res.body;
-  //        done();
-  //      });
-  // });
+  let user = null
+  before((done) => {
+    console.log('waiting');
+    setTimeout(() => {}, 10000);
+    request.post('localhost:9000/users')
+       .send({username: 'USERNAME', password: 'USERPASS'})
+       .end((err, res) => {
+         user = res.body;
+         done();
+       });
+  });
   it('should return 404 for an unregistered route', function(done) {
     request.get('http://localhost:9000/stuff')
     .end((err, res) => {
-      console.log(err);
-      console.log(res);
       expect(res.status).to.equal(404);
       done();
     });
   });
 
 
+
 // test POST errors/messages
   describe('testing POST /users for response 200', function(){
-    it('should return a user', function(done){
+    it('does not include the password', function(done){
       request.post('localhost:9000/users')
       .send({username: 'USERNAME'})
       .end((err, res) => {
         if (err) return done(err);
         expect(res.status).to.equal(200);
-        expect(res.body.username).to.equal('USERNAME');
         done();
       });
     });
-  //
-    it('should responds with "bad request" for if no body provided or invalid body provided', function(done){
+
+    it('should responds with a 400 level error for if no body provided or invalid body provided', function(done){
       request.post('localhost:9000/users')
-      .send({username: 'USERNAME'})
+      .send({tintin: 'USERNAME'})
       .end((err, res) => {
         expect(res.status).to.equal(404);
         done();
@@ -51,38 +49,29 @@ describe('testing user rotues', function(){
 
 
 // testing GET errors/messages
-  // describe('testing GET /suers respones', function(){
-  //   it('provided an id it should return a user', function(done){
-  //     request.get(`localhost:9000/users/${user.id}`)
-  //     .end((err, res) => {
-  //       if (err) return done(err);
-  //       expect(res.status).to.equal(200);
-  //       expect(res.body.username).to.equal('USERNAME');
-  //       expect(res.body.password).to.equal('OWNER STYLE');
-  //       owner = res.body;
-  //       done();
-  //     });
-  //   });
-  //
-  //   it('should return a 400 bad request error if no id was provided', function(done){
-  //     request.get('localhost:9000/owners/')
-  //     .end((err, res) => {
-  //       expect(res.status).to.equal(400);
-  //       done();
-  //     });
-  //   });
-  //
-  //   it('should return a 404 error if the id was not found' , function(done){
-  //     request.get('localhost:9000/owners/lemmy')
-  //     .end((err, res) => {
-  //       expect(res.status).to.equal(404);
-  //       // expect(res.text).to.equal('Not Found');
-  //       done();
-  //     });
-  //   });
-  //
-  //
-  // });
+  describe('testing GET /users respones', function(){
+    it('provided an id it should return a user', function(done){
+      request.get('localhost:9000/users/:id')
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.status).to.equal(200);
+        expect(res.body.username).to.equal('USERNAME');
+        expect(res.body.password).to.equal('USERPASS');
+        user = res.body;
+        done();
+      });
+    });
+
+    it('should return a 400 bad request error if no password was provided', function(done){
+      request.get('localhost:9000/owners/aubie')
+      .end((err, res) => {
+        expect(res.status).to.equal(400);
+        done();
+      });
+    });
+
+
+  });
 
 //end of file
 });
