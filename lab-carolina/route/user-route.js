3d76cd13424a746ca99492acfb0829ec71b01e6d
Carolina Vasquez-Ceja <cejac10@gmail.com>
2017-01-02 23:32:41 -0800

    scaffolding

---
+++ lab-carolina/route/user-route.js
@@ -0,0 +1,3 @@
+'use strict';
+
+const User = require('../model/user.js')
---
+++ lab-carolina/server.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const express = require('express');
+const mongoose = require('mongoose');
+const jsonParser = require('body-parser').json();
+const morgan = require('morgan');
