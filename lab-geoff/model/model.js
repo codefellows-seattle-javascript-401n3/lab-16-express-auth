9ac73a5e55fa20ef82abf527bbe170206e1db8ea
Geoffrey Cox <gcox3d@gmail.com>
2017-01-03 02:42:21 -0800

    updated model method

--- lab-geoff/model/model.js
+++ lab-geoff/model/model.js
@@ -1,23 +1,22 @@
-//username(unique)
-//password (hashed)
-//e-mail(required)
 'use strict';
 let mongoose = require('mongoose');
 let bcrypt = require('bcrypt');
 let Schema = mongoose.Schema;
 
-let userSchema = new Schema({
+let userSchema = Schema({
   username: {type: String, required: true, unique: true},
-  password:{type: String},
+  password: {type: String, required: true},
   email: {type: String, required: true}
 });
 
 userSchema.methods.hashPass = function(password) {
-  bcrypt.hash(password, 16)
-  .then(function(hash) {
-    this.password = hash;
-  })
-  .catch(); //<---here
+  return new Promise((resolve, reject) => {
+    bcrypt.hash(password, 10, (err, hash) => {
+      if(err) return reject(err);
+      this.password = hash;
+      resolve(this);
+    });
+  });
 };
 
 module.exports = mongoose.model('User', userSchema);
