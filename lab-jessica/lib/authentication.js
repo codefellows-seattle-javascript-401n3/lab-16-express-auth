ee859e614451f3c3c324586fa9a775355b9fae16
JESSICA VASQUEZ-SOLTERO <JMICHELLEVS@GMAIL.COM>
2017-01-09 15:56:22 -0800

    Added some error handling, user can now only access their own info

--- lab-jessica/lib/authentication.js
+++ lab-jessica/lib/authentication.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const createError = require('http-errors');
 const User = require('../models/user.js');
 
 module.exports = (req, res, next) => {
@@ -10,9 +11,19 @@ module.exports = (req, res, next) => {
   const base64String = auth.split('Basic ')[1];
   const [username, password] = new Buffer(base64String, 'base64').toString().split(':');
 
+
   User.findOne({username: username})
     .then(user => {
-      return user.comparePasswords(password);
+
+      if(!user) {
+        return Promise.reject(createError(404, 'User not found in database'));
+      }
+
+      if(req.params.id == user._id) {
+        return user.comparePasswords(password);
+      }
+
+      return Promise.reject(createError(401, 'You are not authorized'));
     })
     .then(user => {
       req.user = user;
