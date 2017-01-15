6d39557002dd97259e00c6b68f0cb60b28111674
Jessica Vasquez-Soltero <jmichellevs@gmail.com>
2017-01-15 15:40:46 -0800

    Fixed linter issue

--- lab-jessica/routes/auth-routes.js
+++ lab-jessica/routes/auth-routes.js
@@ -6,7 +6,7 @@ const basicAuth = require('../lib/basic-authentication.js');
 const router = module.exports = new Router();
 
 router.post('/login', basicAuth, (req, res) => {
-    req.user.generateToken()
+  req.user.generateToken()
     .then(token => res.json(token))
     .catch(err => console.error(err));
 });
