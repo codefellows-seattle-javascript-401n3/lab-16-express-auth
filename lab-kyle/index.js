7667f714ed4f5e250106e15712935f0cdb52144b
Kyle Winckler <kyle.winckler@gmail.com>
2017-01-15 13:05:21 -0800

    testing

--- lab-kyle/index.js
+++ lab-kyle/index.js
@@ -18,4 +18,8 @@ app.use(morgan('dev'))
 
 require('./route/user-routes')(app)
 
-app.listen(PORT, () => console.log(`Server started on port: ${PORT}`))
+module.exports = app
+
+if (require.main === module) {
+  app.listen(PORT, () => console.log(`Server started on port: ${PORT}`))
+}
