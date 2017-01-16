e58463563fb69a243161a6382a7b20ef9eab7002
Jessica Vasquez-Soltero <jmichellevs@gmail.com>
2017-01-16 14:10:08 -0800

    Slight changes

--- lab-jessica/lib/bearer-authentication.js
+++ lab-jessica/lib/bearer-authentication.js
@@ -1,33 +1,27 @@
 const jwt = require('jsonwebtoken');
+const createError = require('http-errors');
+
 const User = require('../models/user.js');
 
 module.exports = (req, res, next) => {
+  const auth = req.headers.authorization;
+  if(!auth) return next(createError(401, 'authorization header required'));
+
   let [method, token] = req.headers.authorization.split(' ');
+  if(!token) return next(createError(401, 'token required'));
 
-  if(method.toLowerCase() !== 'bearer') {
-      // handle as required for you domain
-    res.send('not bearer');
-    return;
-  }
+  if(method.toLowerCase() !== 'bearer') return next(createError(401, 'bearer authentication required'));
 
-  let decoded = null;
+  jwt.verify(token, process.env.SECRET || 'DEV', (err, decoded) => {
+    if(err) return next(err);
 
-  try {
-    decoded = jwt.verify(token, process.env.SECRET || 'DEV');
-  }
-  catch (e) {
-    return res.status(401).json({msg: 'Authentication Error'});
-  }
-
-  User.findById(decoded.id)
-    .populate('courses')
-    .exec( (err, user) => {
-      if (err) throw Error;
-      req.user = user;
-      next();
-    })
-    .catch(err => {
-      console.error(err);
-      res.json({msg: 'Authentication Error from Bearer Middleware'});
-    });
+    User.findById(decoded.id)
+      .then(user => {
+        req.user = user;
+        next();
+      })
+      .catch(err => {
+        next(createError(401, err.message));
+      });
+  });
 };
