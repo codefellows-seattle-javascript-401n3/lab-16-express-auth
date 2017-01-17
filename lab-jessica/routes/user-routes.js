2e5fd8be857e6c1b51a716182647242b6d2d44f5
Jessica Vasquez-Soltero <jmichellevs@gmail.com>
2017-01-16 16:32:03 -0800

    moved post users to signup in auth-routes. Current GET route does not work

--- lab-jessica/routes/user-routes.js
+++ lab-jessica/routes/user-routes.js
@@ -8,19 +8,6 @@ const bearerAuth = require('../lib/bearer-authentication.js');
 
 const router = module.exports = new Router();
 
-router.post('/users', (req, res) => {
-
-  const user = new User(req.body);
-
-  user.hashPassword(user.password)
-    .then(user => user.save())
-    .then(user => res.json({username: user.username, email: user.email, _id: user._id}))
-    .catch(err => {
-      console.error(err);
-      res.status(401).send('bad request' + '\n');
-    });
-});
-
 router.get('/users', bearerAuth, (req, res) => {
   if (req.user) {
     res.json({username: req.user.username, email: req.user.email, _id: req.user._id});
